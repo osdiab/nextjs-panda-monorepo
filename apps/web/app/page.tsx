20a2e3311396c3d1f3a7c08d1a873b6a18fe6df0
Omar Diab <me@omardiab.com>
2023-06-27 13:04:18 +0900

    setup panda

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,16 @@
 import { Button, Header } from "ui";
+import { hstack } from "../styled-system/patterns";
+import { css, cx } from "../styled-system/css";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
+      <div className={cx(hstack({gap: "12"}), css({color: "red"}))}>
+        <span>Hi</span>
+        <span>Hi</span>
+        <span>Hi</span>
+      </div>
       <Button />
     </>
   );
